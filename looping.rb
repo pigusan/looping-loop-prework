b4f11bdb14c0be907a9424a821c3a278102655f9
github username <github email address>
2017-09-20 00:05:33 -0400

    Loopy doop

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def looping
-	#your code here
+    counter = 0
+    loop do
+        counter += 1
+        puts "Hello world. This is the #{counter} iteration."
+        if counter >= 10
+            break
+        end
+    end
 end
 
-#call your method here
+looping
